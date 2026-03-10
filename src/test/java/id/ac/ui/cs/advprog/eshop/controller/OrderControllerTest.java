85e69b014568ba9ead83ebbeb1e72afe77fbf66e
NaufalFadliR <naufalfr2626@gmail.com>
2026-03-10 20:17:18 +0700

    [RED] Add tests for order/pay endpoints in OrderController

--- src/test/java/id/ac/ui/cs/advprog/eshop/controller/OrderControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/OrderControllerTest.java
@@ -1,18 +1,26 @@
 package id.ac.ui.cs.advprog.eshop.controller;
 
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import id.ac.ui.cs.advprog.eshop.service.PaymentService;
 import id.ac.ui.cs.advprog.eshop.service.OrderService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 
 @WebMvcTest(controllers = OrderController.class)
 class OrderControllerTest {
@@ -21,6 +29,9 @@ class OrderControllerTest {
 
     @MockBean
     private OrderService orderService;
+
+    @MockitoBean
+    private PaymentService paymentService;
 
     @Test
     void testCreateOrderPage() throws Exception {
@@ -45,5 +56,35 @@ class OrderControllerTest {
                 .andExpect(view().name("order/HistoryOrderResult")) // Disesuaikan
                 .andExpect(model().attributeExists("orders"))
                 .andExpect(model().attributeExists("author"));
+    }
+
+    @Test
+    void testPaymentOrderPage() throws Exception {
+        Order order = new Order("order-1", new ArrayList<>(), 123L, "Safira");
+        when(orderService.findById("order-1")).thenReturn(order);
+
+        mockMvc.perform(get("/order/pay/order-1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("order/PaymentOrder"))
+                .andExpect(model().attributeExists("order"));
+    }
+
+    @Test
+    void testPaymentOrderPost() throws Exception {
+        Order order = new Order("order-1", new ArrayList<>(), 123L, "Safira");
+        when(orderService.findById("order-1")).thenReturn(order);
+
+        Map<String, String> paymentData = new HashMap<>();
+        paymentData.put("voucherCode", "ESHOP1234ABC5678");
+        Payment payment = new Payment("payment-1", "VOUCHER", paymentData);
+
+        when(paymentService.addPayment(eq(order), eq("VOUCHER"), any())).thenReturn(payment);
+
+        mockMvc.perform(post("/order/pay/order-1")
+                        .param("method", "VOUCHER")
+                        .param("voucherCode", "ESHOP1234ABC5678"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("order/PaymentOrderResult"))
+                .andExpect(model().attributeExists("payment"));
     }
 }
