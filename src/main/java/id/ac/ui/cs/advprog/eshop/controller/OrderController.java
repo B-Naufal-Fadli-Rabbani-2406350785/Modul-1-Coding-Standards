aa93c8b71cca61d13fb98d04eb744e5bafa00322
NaufalFadliR <naufalfr2626@gmail.com>
2026-03-10 20:00:41 +0700

    [GREEN] Implement OrderController and make tests pass

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/OrderController.java
@@ -0,0 +1,36 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import id.ac.ui.cs.advprog.eshop.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/order")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping("/create")
+    public String createOrderPage() {
+        return "order/CreateOrder"; // Disesuaikan
+    }
+
+    @GetMapping("/history")
+    public String historyOrderPage() {
+        return "order/HistoryOrder"; // Disesuaikan
+    }
+
+    @PostMapping("/history")
+    public String historyOrderPost(@RequestParam String author, Model model) {
+        List<Order> orders = orderService.findAllByAuthor(author);
+        model.addAttribute("orders", orders);
+        model.addAttribute("author", author);
+        return "order/HistoryOrderResult"; // Disesuaikan
+    }
+}
