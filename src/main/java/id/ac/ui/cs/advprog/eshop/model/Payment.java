2d2d8235a8bdd16d755bb550acff24c69c0ff013
NaufalFadliR <naufalfr2626@gmail.com>
2026-03-10 19:10:57 +0700

    [GREEN] Implement Payment model logic for Voucher and COD

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -11,6 +11,40 @@ public class Payment {
     Map<String, String> paymentData;
 
     public Payment(String id, String method, Map<String, String> paymentData) {
-        // Akan kita isi logikanya di fase GREEN nanti
+        this.id = id;
+        this.method = method;
+        this.paymentData = paymentData;
+
+        if ("VOUCHER".equals(method)) {
+            String voucherCode = paymentData.get("voucherCode");
+            if (voucherCode != null && voucherCode.length() == 16 && voucherCode.startsWith("ESHOP")) {
+                int numCount = 0;
+                for (char c : voucherCode.toCharArray()) {
+                    if (Character.isDigit(c)) {
+                        numCount++;
+                    }
+                }
+
+                if (numCount == 8) {
+                    this.status = "SUCCESS";
+                } else {
+                    this.status = "REJECTED";
+                }
+            } else {
+                this.status = "REJECTED";
+            }
+        } else if ("CASH_ON_DELIVERY".equals(method)) {
+            String address = paymentData.get("address");
+            String deliveryFee = paymentData.get("deliveryFee");
+
+            if (address == null || address.trim().isEmpty() ||
+                    deliveryFee == null || deliveryFee.trim().isEmpty()) {
+                this.status = "REJECTED";
+            } else {
+                this.status = "SUCCESS";
+            }
+        } else {
+            this.status = "REJECTED";
+        }
     }
 }
