0eca17b4759373fa7228d0d198f39794c234ea14
NaufalFadliR <naufalfr2626@gmail.com>
2026-03-10 20:18:46 +0700

    [GREEN] Implement order/pay endpoints in OrderController

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/OrderController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/OrderController.java
@@ -2,12 +2,15 @@ package id.ac.ui.cs.advprog.eshop.controller;
 
 import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.service.OrderService;
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import id.ac.ui.cs.advprog.eshop.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/order")
@@ -15,6 +18,9 @@ public class OrderController {
 
     @Autowired
     private OrderService orderService;
+
+    @Autowired
+    private PaymentService paymentService;
 
     @GetMapping("/create")
     public String createOrderPage() {
@@ -31,6 +37,24 @@ public class OrderController {
         List<Order> orders = orderService.findAllByAuthor(author);
         model.addAttribute("orders", orders);
         model.addAttribute("author", author);
-        return "order/HistoryOrderResult"; // Disesuaikan
+        return "order/HistoryOrderResult";
+    }
+
+    @GetMapping("/pay/{orderId}")
+    public String paymentOrderPage(@PathVariable String orderId, Model model) {
+        Order order = orderService.findById(orderId);
+        model.addAttribute("order", order);
+        return "order/PaymentOrder";
+    }
+
+    @PostMapping("/pay/{orderId}")
+    public String paymentOrderPost(@PathVariable String orderId,
+                                   @RequestParam String method,
+                                   @RequestParam Map<String, String> paymentData,
+                                   Model model) {
+        Order order = orderService.findById(orderId);
+        Payment payment = paymentService.addPayment(order, method, paymentData);
+        model.addAttribute("payment", payment);
+        return "order/PaymentOrderResult";
     }
 }
